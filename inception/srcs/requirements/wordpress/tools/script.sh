fd49f890cc79c1f360a237e08069dfb10abbfa52
Junseo Park <junsepar@c3r5s5.42seoul.kr>
2024-03-29 18:38:48 +0900

    modified compose, wp_dockerfile, script

--- inception/srcs/requirements/wordpress/tools/script.sh
+++ inception/srcs/requirements/wordpress/tools/script.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f "/var/www/html/wp-config.php" ]; then
     cd /var/www/html
-    pwd
+
     wp core download --allow-root
     wp config create --allow-root --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --dbhost=mariadb --path=/var/www/html
     wp core install --allow-root --url=localhost --title=wordpress --admin_user=${WP_ADMIN_ID} --admin_password=${WP_ADMIN_PW} --admin_email=email@example.com --path=/var/www/html
